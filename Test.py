fdb4361d58914926ba7d114776e11bf75d12e0a8
PRASAD1912200 <prasadtavatari@gmail.com>
2026-09-21 14:05:47 +0530

    flight_user_input

--- Test.py
+++ Test.py
@@ -1,4 +1,8 @@
 from tools.tavily_tool import tavily_serach
+from tools.flight_tool import search_flights
 
-res = tavily_serach("Best hotels in India")
+res = search_flights("Plan a 7 days Japan trip from Bangladesh")
 print(res)
+
+# res = tavily_serach("Best hotels in India")
+# print(res)
