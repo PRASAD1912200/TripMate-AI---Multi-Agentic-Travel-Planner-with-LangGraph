cf84470d82c98675122a35ef96e418b97a2f4564
PRASAD1912200 <prasadtavatari@gmail.com>
2026-09-21 20:20:44 +0530

    backend_langgraph

--- tools/tavily_tool.py
+++ tools/tavily_tool.py
@@ -8,7 +8,7 @@ client = TavilyClient (
     api_key=os.getenv("TAVILY_API_KEY")
 )
 
-def tavily_serach(query):
+def tavily_search(query):
     response = client.search(
         query= query,
         max_results=5
